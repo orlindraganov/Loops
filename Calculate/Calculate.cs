434cec7de058ad7ba92af745afbe90e4cbc85c95
orlindraganov <orlindraganov@gmail.com>
2016-04-25 22:48:14 +0300

    Solved 5, working on Calculate Again

--- Calculate/Calculate.cs
+++ Calculate/Calculate.cs
@@ -10,9 +10,9 @@ class Calculate
         int n = int.Parse(Console.ReadLine());
         double x = double.Parse(Console.ReadLine());
         double sum = 1;
-        int nFactorial = 1;
+        long nFactorial = 1;
 
-        for (int i = 1; i < n; i++)
+        for (int i = 1; i <= n; i++)
         {
             nFactorial *= i;
             sum += nFactorial / Math.Pow(x, i);
